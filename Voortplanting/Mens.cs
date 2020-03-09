aa09f5be7daafeab481fcd2f0fe96317ffce8bd4
Shourov <shourov.ahmed@student.ap.be>
2020-03-09 12:12:58 +0100

    Klasse mens gemaakt + properties toegevoegd

---
+++ Voortplanting/Mens.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voortplanting
+{
+    enum Geslacht { Man, Vrouw}
+    enum Oogkleur { Blauw, Bruin, Groen, Rood}
+    class Mens
+    {
+        public Geslacht Geslacht { get; set; }
+        public Oogkleur Oogkleur { get; set; }
+
+        private double maxLengte;
+
+        public double MaxLengte
+        {
+            get { return maxLengte; }
+            set { maxLengte = value; }
+        }
+
+
+
+
+
+    }
+}
