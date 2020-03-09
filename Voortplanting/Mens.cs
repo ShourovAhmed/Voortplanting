fa6babf9d81c623692762cc6a9201ab6d2e622bb
Shourov <shourov.ahmed@student.ap.be>
2020-03-09 12:22:54 +0100

    Default constructor + sommige props op private gezet

--- Voortplanting/Mens.cs
+++ Voortplanting/Mens.cs
@@ -4,19 +4,47 @@ using System.Text;
 
 namespace Voortplanting
 {
-    enum Geslacht { Man, Vrouw}
-    enum Oogkleur { Blauw, Bruin, Groen, Rood}
+    enum Geslacht { Man, Vrouw }
+    enum Oogkleur { Blauw, Bruin, Groen, Rood }
     class Mens
     {
-        public Geslacht Geslacht { get; set; }
-        public Oogkleur Oogkleur { get; set; }
+        static Random r = new Random();
+        //Default constructor
+        public Mens()
+        {
+            if (r.Next(0, 2) == 0)
+            { Geslacht = Geslacht.Vrouw; }
+
+            else
+            { Geslacht = Geslacht.Man; }
+
+            Oogkleur = (Oogkleur)r.Next(0, 4);//is een enum dus evenveel kans
+
+            int percentlengte = r.Next(0, 100);
+            if (percentlengte < 5)
+            {
+                MaxLengte = r.Next(40, 151);
+            }
+            else if (percentlengte >= 95)
+            {
+                MaxLengte = r.Next(151, 210);
+            }
+            else
+            {
+                MaxLengte = r.Next(151, 210);
+            }
+        }
+
+        //Eigenschappen
+        public Geslacht Geslacht { get; private set; }//private set omdat je niet opeens je mens andere oogkleur of geslacht kan geven (voorlopig)
+        public Oogkleur Oogkleur { get; private set; }
 
         private double maxLengte;
 
         public double MaxLengte
         {
             get { return maxLengte; }
-            set { maxLengte = value; }
+            private set { if (value >= 30) maxLengte = value; }
         }
 
 
